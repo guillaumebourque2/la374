3041af09949277cbd391e7428847480654d91f52
Guillaume Bourque <guillaumebourque2@gmail.com>
2016-12-06 18:49:59 -0500

    Enable EN/FR button to toggle text language

--- la374js.js
+++ la374js.js
@@ -1,37 +1,13 @@
 //Switching between languages on narrow devices (smartphones)
-function language() {
-	//Desactivate any previous click on accordion buttons
-	$(".btnRow .accordionBtn").removeClass("active");			
-	$(".accordionPanel").slideUp(0);
+$("#language").click(function() {
+	//Switch text to the other language
+		$("span[lang|='en']").toggle(0);
+		$("span[lang|='fr']").toggle(0);
+});
 
-	language2()
-}
-function language2() {
-	//Switch news tile and accordions buttons to the other language
-	if ($("button.language").html() === "&nbsp;English&nbsp;") {
-		//French to English
-		$(".btnRow [lang|='en']").css({"display": "block"});
-		$(".news:lang(en)").css({"display": "block"});
-
-		$(".btnRow [lang|='fr']").css({"display": "none"});
-		$(".news:lang(fr)").css({"display": "none"});
-
-		$("button.language").html("Français");
-
-	} else {
-		//English to French
-		$(".btnRow [lang|='fr']").css({"display": "block"});
-		$(".news:lang(fr)").css({"display": "block"});
-
-		$(".btnRow [lang|='en']").css({"display": "none"});
-		$(".news:lang(en)").css({"display": "none"});
-
-		$("button.language").html("&nbsp;English&nbsp;");
-	}
-}
-
-//Switching automatically to French if coming from a project page in French
+//Switching automatically to French if coming from a project page in French, 
+//or in any other cases where the url includes "?lang=fr"
 if (/[?]lang=fr/.test(window.location.href) ) {
-		language2();
+	document.getElementById("language").click();
 }
 
